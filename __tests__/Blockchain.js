557c07464b7cdeec4180c68b059796ff59361f3c
Matias Olivera <moliverafreire@gmail.com>
2017-11-06 10:32:27 -0300

    Add basic Blockchain implementation

---
+++ __tests__/Blockchain.js
@@ -0,0 +1,67 @@
+const Block = require('../lib/Block');
+const Blockchain = require('../lib/Blockchain');
+
+describe('Blockchain', () => {
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
+
+  test('has a chain of blocks which is an Array', () => {
+    expect(Array.isArray(blockchain.chain)).toBe(true);
+  });
+
+  test('starts with the genesis block', () => {
+    expect(blockchain.chain[0]).toEqual(Blockchain.createGenesisBlock());
+  });
+
+  test('knows what the last block is', () => {
+    const block = new Block(0, null, 0, 'foo');
+    blockchain.chain.push(block);
+    expect(blockchain.getLastBlock()).toEqual(block);
+  });
+
+  test('allows adding a valid block to the chain', () => {
+    const lastBlock = blockchain.getLastBlock();
+    const index = lastBlock.index + 1;
+    const timestamp = Date.now() / 1000;
+    const lastBlockHash = lastBlock.hash;
+    const block = new Block(index, lastBlockHash, timestamp, 'foo');
+    blockchain.addBlock(block);
+    expect(blockchain.getLastBlock()).toEqual(block);
+  });
+
+  test('rejects invalid blocks', () => {
+    const lastBlock = blockchain.getLastBlock();
+    const invalidBlock = new Block(0, null, 0, 'foo');
+    blockchain.addBlock(invalidBlock);
+    expect(blockchain.getLastBlock()).toEqual(lastBlock);
+    expect(lastBlock).not.toEqual(invalidBlock);
+  });
+
+  test('is valid', () => {
+    expect(blockchain.isValid()).toBe(true);
+  });
+
+  describe('with no blocks', () => {
+    test('is not valid', () => {
+      blockchain.chain = [];
+      expect(blockchain.isValid()).toBe(false);
+    });
+  });
+
+  describe('with first block different than genesis block', () => {
+    test('is not valid', () => {
+      blockchain.chain = [new Block(0, null, 0, 'foo')];
+      expect(blockchain.isValid()).toBe(false);
+    });
+  });
+
+  describe('with invalid block', () => {
+    test('is not valid', () => {
+      blockchain.chain.push(new Block(0, null, 0, 'foo'));
+      expect(blockchain.isValid()).toBe(false);
+    });
+  });
+});
